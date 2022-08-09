20dcc151c65754830c37e2342d39eaf768e3d5bd
TapasSahoo78 <tapasbca2015@gmail.com>
2022-08-09 18:49:49 +0530

    mern stack checkbox value fetch

---
+++ backend/models/Checkbox.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+let checkboxModel = new Schema({
+    checkbox: {
+        type: String
+    },
+}, {
+    collection: 'checkboxes'
+});
+module.exports = mongoose.model('CheckboxModel', checkboxModel);
